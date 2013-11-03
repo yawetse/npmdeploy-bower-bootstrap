3aaa25fd2e08b398b39e4e5d2c8c377ef253d3dc
Yaw Joseph Etse <yaw.etse@gmail.com>
2013-11-02 20:54:30 -0400

    npm deployable bootstrap

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-bower-bootstrap
-===============
+# bower_bootstrap
 
 Sleek, intuitive, and powerful mobile first front-end framework for faster and easier web development.
+
+deployable via NPM
---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * bower_bootstrap
+ * http://github.amexpub.com/modules/bower_bootstrap
+ *
+ * Copyright (c) 2013 AmexPub. All rights reserved.
+ */
+
+module.exports = require('./lib/bower_bootstrap');
